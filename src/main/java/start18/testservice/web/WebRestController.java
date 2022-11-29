c6c7f67545128a3fbb7c19cac289f337a7bfd573
Ganghee Lee <gh_5226@naver.com>
2022-11-29 22:12:05 +0900

    캡스톤디자인과창업프로젝트A 스타트 18팀의 테스트 서버입니다.

---
+++ src/main/java/start18/testservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package start18.testservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
